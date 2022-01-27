958fc0738700d794f3b3305c8bf95c3c643efaee
jktham-sf <jonas.tham@gmx.ch>
2022-01-27 10:37:45 +0100

    Add textures, texture blending

--- opengl-glfw/main.cpp
+++ opengl-glfw/main.cpp
@@ -7,7 +7,10 @@
 #include <iostream>
 
 #include "shader.h"
+#include "stb_image.h"
 
+
+float mixValue = 0.5f;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);
@@ -16,6 +19,29 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 void processInput(GLFWwindow* window) {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
+
+	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
+		mixValue += 0.001f;
+	}
+	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
+		mixValue -= 0.001f;
+	}
+	if (mixValue < 0.0f) {
+		mixValue = 0.0f;
+	}
+	if (mixValue > 1.0f) {
+		mixValue = 1.0f;
+	}
+}
+
+float changeBlend(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
+		return 0.1f;
+	} else if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
+		return -0.1f;
+	} else {
+		return 0.0f;
+	}
 }
 
 int main() {
@@ -48,14 +74,17 @@ int main() {
 
 	// vertex data
 	float vertices[] = {
-		 0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,
-		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,
-		 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f
+		// positions          // colors           // texture coords
+		 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+		 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
 	};
 
 	// index data
 	unsigned int indices[] = {
-		0, 1, 2
+		0, 1, 2,
+		2, 3, 0
 	};
 
 	// vertex array object
@@ -78,31 +107,89 @@ int main() {
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
+	// texture coordinates
+	float texCoords[] = {
+		0.0f, 0.0f,
+		1.0f, 0.0f,
+		0.5f, 1.0f
+	};
+
+	// texture attributes
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	// load textures
+	int width, height, nrChannels;
+	unsigned char* data;
+
+	stbi_set_flip_vertically_on_load(true);
+
+	unsigned int texture1;
+	glGenTextures(1, &texture1);
+	glBindTexture(GL_TEXTURE_2D, texture1);
+
+	data = stbi_load("wood.jpg", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	stbi_image_free(data);
+
+	unsigned int texture2;
+	glGenTextures(1, &texture2);
+	glBindTexture(GL_TEXTURE_2D, texture2);
+
+	data = stbi_load("brick.jpg", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	stbi_image_free(data);
+
 	// vertex attribute (position)
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
 	// vertex attribute (color)
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
+
+	// vertex attribute (texture)
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
 
 	// shader program
 	Shader shaderProgram("shader.vert", "shader.frag");
 
+	// set texture uniforms
+	shaderProgram.use();
+	glUniform1i(glGetUniformLocation(shaderProgram.ID, "texture1"), 0);
+	shaderProgram.setInt("texture2", 1);
+
 	// render loop
 	while (!glfwWindowShouldClose(window)) {
-		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
 		processInput(window);
 
 		glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		shaderProgram.use();
+		shaderProgram.setFloat("mixValue", mixValue);
+
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture1);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture2);
+
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glBindVertexArray(VAO);
 		glDrawElements(GL_TRIANGLES, sizeof(indices), GL_UNSIGNED_INT, 0);
-		glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
--- opengl-glfw/shader.frag
+++ opengl-glfw/shader.frag
@@ -1,9 +1,15 @@
 #version 460 core
 
-in vec3 vertColor;
-
 out vec4 FragColor;
+  
+in vec3 ourColor;
+in vec2 TexCoord;
 
-void main() {
-	FragColor = vec4(vertColor, 1.0);
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+uniform float mixValue;
+
+void main()
+{
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), mixValue);
 }
